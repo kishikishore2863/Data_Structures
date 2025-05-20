21a1405cfb6b75b3d7f11d65196edd289e1fa9b3
kishikishore2863 <kishikishore.2863@gmail.com>
2025-05-20 23:44:26 +0530

    LCA

--- src/main/java/testing/Test.java
+++ src/main/java/testing/Test.java
@@ -6,41 +6,38 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        ListNode one = new ListNode(1);
-        ListNode two = new ListNode(2);
-        ListNode three = new ListNode(3);
-        ListNode four = new ListNode(4);
-        ListNode five = new ListNode(5);
-        ListNode six = new ListNode(6);
-
-        ListNode head = one;
-        one.next = two;
-        two.next = three;
-        three.next = four;
-        four.next = five;
-        five.next = six;
-
-        ListNode next = head.next.next;
-        ListNode first = head;
-        ListNode second = head.next;
-
-        second.next = first;
-        first.next = next;
-
-        ListNode prev = new ListNode(0); // dummy node
-        prev.next = second;
-
-
-
-
-        ListNode it = prev;
-        while (it!=null){
-            System.out.print(it.val);
-            it=it.next;
-        }
-
-
-
+//        ListNode one = new ListNode(1);
+//        ListNode two = new ListNode(2);
+//        ListNode three = new ListNode(3);
+//        ListNode four = new ListNode(4);
+//        ListNode five = new ListNode(5);
+//        ListNode six = new ListNode(6);
+//
+//        ListNode head = one;
+//        one.next = two;
+//        two.next = three;
+//        three.next = four;
+//        four.next = five;
+//        five.next = six;
+//
+//        ListNode next = head.next.next;
+//        ListNode first = head;
+//        ListNode second = head.next;
+//
+//        second.next = first;
+//        first.next = next;
+//
+//        ListNode prev = new ListNode(0); // dummy node
+//        prev.next = second;
+//
+//
+//
+//
+//        ListNode it = prev;
+//        while (it!=null){
+//            System.out.print(it.val);
+//            it=it.next;
+//        }
 
     }
 }
