a6f6f2ebdbfc5fbb4db00c0c2839d4d8f49f51e8
kishikishore2863 <kishikishore.2863@gmail.com>
2025-06-08 23:07:49 +0530

    testing

--- src/main/java/contest/biweekly_158/Q1.java
+++ src/main/java/contest/biweekly_158/Q1.java
@@ -7,7 +7,15 @@ public class Q1{
         int [] x = {1,2,1,3,2};
         int [] y = {5,3,4,6,2};
 
-        System.out.println(maxSumDistinctTriplet(x,y));
+//        System.out.println(maxSumDistinctTriplet(x,y));
+        int number =-123;
+        int res= reverse(number);
+        System.out.println(res);
+        System.out.println(Integer.MAX_VALUE);
+        int res1 =2147483647;int com =1534236469;
+        if(com<res1) System.out.println(0);
+
+
     }
 
     public static int maxSumDistinctTriplet(int[] x, int[] y) {
@@ -24,6 +32,22 @@ public class Q1{
 
 
         return topY.get(0) + topY.get(1) + topY.get(2);
+
+    }
+
+    public static int reverse(int x) {
+        boolean isNegative=false;
+        if(x<0){
+            isNegative = true;
+        }
+        int reversed = 0;
+        x= Math.abs(x);
+        while(x>0){
+            int lastDigit = x%10;
+            reversed =reversed*10+lastDigit;
+            x = x/10;
+        }
+        return isNegative?-reversed:reversed;
 
     }
 }
