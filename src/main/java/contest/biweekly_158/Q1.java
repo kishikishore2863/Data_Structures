529e7255e8f4228cc05ff4bdf9a23764240a71b6
kishikishore2863 <kishikishore.2863@gmail.com>
2025-06-09 23:34:40 +0530

    backtracking

--- src/main/java/contest/biweekly_158/Q1.java
+++ src/main/java/contest/biweekly_158/Q1.java
@@ -16,6 +16,10 @@ public class Q1{
         if(com<res1) System.out.println(0);
 
 
+        System.out.println("Subsets:");
+        generateSubsets(new int[]{1, 2, 3});
+        System.out.println("Permutations:");
+        generatePermutations(new int[]{1, 2, 3});
     }
 
     public static int maxSumDistinctTriplet(int[] x, int[] y) {
@@ -49,5 +53,45 @@ public class Q1{
         }
         return isNegative?-reversed:reversed;
 
+    }
+
+    public static void generateSubsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrackSubsets(nums, 0, new ArrayList<>(), result);
+        for (List<Integer> subset : result) {
+            System.out.println(subset);
+        }
+    }
+
+    private static void backtrackSubsets(int[] nums, int index, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+        for (int i = index; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrackSubsets(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public static void generatePermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrackPermutations(nums, new ArrayList<>(), new boolean[nums.length], result);
+        for (List<Integer> perm : result) {
+            System.out.println(perm);
+        }
+    }
+
+    private static void backtrackPermutations(int[] nums, List<Integer> path, boolean[] used, List<List<Integer>> result) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            path.add(nums[i]);
+            backtrackPermutations(nums, path, used, result);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
     }
 }
