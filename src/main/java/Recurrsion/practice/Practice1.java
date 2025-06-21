5e3e5d7eb8dcbb80a18d8ef2a3f18be5bc048889
kishikishore2863 <kishikishore.2863@gmail.com>
2025-06-21 23:58:06 +0530

    two pointers

--- src/main/java/Recurrsion/practice/Practice1.java
+++ src/main/java/Recurrsion/practice/Practice1.java
@@ -1,6 +1,9 @@
 package Recurrsion.practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -22,8 +25,16 @@ public class Practice1 {
 //        String s = "abcde";
 //        reverseString(s,0);
 
-        int [] people = {3,2,2,1};
-        System.out.println(numRescueBoats(people,3));
+//        int [] people = {3,2,2,1};
+//        System.out.println(numRescueBoats(people,3));
+
+//        int [] skill = {1,1,2,3};
+//        System.out.println(dividePlayers(skill));
+
+        String[] event1 ={"01:15","02:00"};
+        String[] event2 ={"02:00","03:00"};
+       boolean res = haveConflict(event1,event2);
+        System.out.println(res);
     }
 
     public static void print1toN(int n){
@@ -95,5 +106,89 @@ public class Practice1 {
             }
         }
         return boat;
+    }
+
+    public static long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        List<List<Integer>> container = new ArrayList<>();
+        int start =0;
+        int end =skill.length-1;
+        while(start<=end){
+            List<Integer> store = new ArrayList<>();
+            store.add(skill[start]);
+            store.add(skill[end]);
+            container.add(store);
+            start++;
+            end--;
+        }
+        System.out.println(container);
+
+        List<Integer> productOfTeam = new ArrayList<>();
+        List<Integer> sumOfTeam = new ArrayList<>();
+        for(List<Integer> team:container){
+            int prod =1;
+            int sum = 0;
+            for(int i=0; i<team.size(); i++){
+                prod = prod * team.get(i);
+                sum = sum + team.get(i);
+            }
+            sumOfTeam.add(sum);
+            productOfTeam.add(prod);
+        }
+        int comp =sumOfTeam.get(0);
+        for(Integer s:sumOfTeam){
+            if(comp != s){
+                return -1;
+            }
+        }
+
+        System.out.println(productOfTeam);
+        long sum =0;
+       for(Integer i:productOfTeam){
+           sum = sum+i;
+       }
+       return sum;
+
+    }
+
+    public boolean haveConflict1(String[] event1, String[] event2) {
+        int event1int= 0;
+        int event2int=0;
+
+        for(int i=0; i<event1[1].length(); i++){
+            if(event1[1].charAt(i)>='0' && event1[1].charAt(i)<='9'){
+                event1int = event1int*10+event1[1].charAt(i);
+            }
+        }
+
+        for(int i=0; i<event2[0].length(); i++){
+            char event2ref = event2[0].charAt(i);
+            if(event2ref>='0' && event2ref<='9'){
+                event2int = event2int*10+event2ref;
+            }
+        }
+        System.out.println(event1int);
+        System.out.println(event2int);
+
+        if(event2int>event1int){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean haveConflict(String[] event1, String[] event2) {
+        int event1Start = toMinutes(event1[0]);
+        int event1End = toMinutes(event1[1]);
+        int event2Start = toMinutes(event2[0]);
+        int event2End = toMinutes(event2[1]);
+
+        return !(event1End < event2Start || event2End < event1Start);
+    }
+
+    private static int toMinutes(String time) {
+        String[] parts = time.split(":");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+        return hours * 60 + minutes;
     }
 }
