6e8ed3b6a62cc48a595aa8f8d50dc4a38772a43d
kishikishore2863 <kishikishore.2863@gmail.com>
2025-07-01 21:54:34 +0530

    playGround

---
+++ src/main/java/heap/HeapPlayGround.java
@@ -0,0 +1,51 @@
+package heap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class HeapPlayGround {
+    public static void main(String[] args) {
+        Queue<Integer> heap = new PriorityQueue<>();
+        heap.add(7);
+        heap.add(10);
+        heap.add(4);
+        heap.add(3);
+        heap.add(20);
+        heap.add(15);
+//        System.out.println(heap);
+
+//        System.out.println(HeapPlayGround.kthLargestElement(new int[]{7,10,4,3,20,15},3));
+
+        printer();
+
+    }
+
+    public static List<Integer> kthLargestElement(int [] arr,int k){
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0; i< arr.length; i++){
+            minHeap.add(arr[i]);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        for(Integer n:minHeap){
+            list.add(n);
+        }
+        return list;
+    }
+
+    public static void printer(){
+
+        for(int i=0; i<6; i++){
+            char c = 'A';
+            for(int j=0; j<6-i; j++){
+                System.out.print(c++);
+            }
+            System.out.println();
+        }
+    }
+
+}
