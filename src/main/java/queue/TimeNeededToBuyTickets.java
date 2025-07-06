ddcd774fc920f0c5303ad3d2d5717999b03e455d
kishikishore2863 <kishikishore.2863@gmail.com>
2025-07-06 22:39:48 +0530

    checkpoint

--- src/main/java/queue/TimeNeededToBuyTickets.java
+++ src/main/java/queue/TimeNeededToBuyTickets.java
@@ -9,26 +9,4 @@ public class TimeNeededToBuyTickets {
     public static void main(String[] args) {
 
     }
-
-//    public int timeRequiredToBuy(int[] tickets, int k) {
-//        Map<Integer,Integer> map = new HashMap<>();
-//        Queue<Map<Integer,Integer>>queue = new LinkedList<>();
-//
-//        for(int i=0; i<tickets.length; i++){
-//            Map<Integer,Integer> mapper = new HashMap<>();
-//            queue.add(mapper.put(i,tickets[i]));
-//        }
-//        int time = 0;
-//        while(!queue.isEmpty()){
-//            time++;
-//            int polled = queue.poll();
-//            polled = polled-1;
-//
-//            if(polled!=0){
-//                queue.add(polled);
-//            }
-//        }
-//
-//        return time;
-    }
 }
