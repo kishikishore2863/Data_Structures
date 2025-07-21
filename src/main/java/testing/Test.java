5e3c707e247a01b6c50ba99a23e0b6db5ecbef70
kishikishore2863 <kishikishore.2863@gmail.com>
2025-07-21 23:39:18 +0530

    rec

--- src/main/java/testing/Test.java
+++ src/main/java/testing/Test.java
@@ -6,38 +6,58 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-//        ListNode one = new ListNode(1);
-//        ListNode two = new ListNode(2);
-//        ListNode three = new ListNode(3);
-//        ListNode four = new ListNode(4);
-//        ListNode five = new ListNode(5);
-//        ListNode six = new ListNode(6);
-//
-//        ListNode head = one;
-//        one.next = two;
-//        two.next = three;
-//        three.next = four;
-//        four.next = five;
-//        five.next = six;
-//
-//        ListNode next = head.next.next;
-//        ListNode first = head;
-//        ListNode second = head.next;
-//
-//        second.next = first;
-//        first.next = next;
-//
-//        ListNode prev = new ListNode(0); // dummy node
-//        prev.next = second;
-//
-//
-//
-//
-//        ListNode it = prev;
-//        while (it!=null){
-//            System.out.print(it.val);
-//            it=it.next;
-//        }
+        Test test = new Test();
+//        test.print1toN(10);
+//        test.printNto1(10);
+        System.out.println(test.fib(10));
+        System.out.println(test.factorial(5));
+        System.out.println(test.sumOfNatural(5));
 
+    }
+
+    public void print1toN(int n){
+        if (n==0)return;
+        print1toN(n-1);
+        System.out.println(n);
+    }
+
+    public void printNto1(int n){
+        if(n==0)return;
+        System.out.println(n);
+        printNto1(n-1);
+    }
+
+    public int fib(int n){
+        if(n==0)return 0;
+        if(n==1)return 1;
+
+        return fib(n-1)+fib(n-2);
+    }
+
+    public int factorial(int n){
+        if(n==0)return 1;
+
+        int prev =factorial(n-1);
+        return prev*n;
+    }
+
+    public int sumOfNatural(int n){
+        if(n==1)return 1;
+
+        int prev = sumOfNatural(n-1);
+        return prev+n;
+    }
+
+    public boolean isPalindrome(String s){
+        String reversed = palindromeReverse(s, 0, "");
+        return s.equals(reversed);
+    }
+
+    private String palindromeReverse(String s, int n, String newS){
+        if(n == s.length()){
+            return newS;
+        }
+        newS = s.charAt(n) + newS;
+        return palindromeReverse(s, n+1, newS);
     }
 }
