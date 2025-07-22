203e394c1e39b96f8a23518252a8f6e8c1e21e8c
kishikishore2863 <kishikishore.2863@gmail.com>
2025-07-22 23:42:07 +0530

    knapsack recursion+memoization

--- src/main/java/Dynamic_Programming/KnapsackMemoization.java
+++ src/main/java/Dynamic_Programming/KnapsackMemoization.java
@@ -4,33 +4,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class KnapsackMemoization {
-//    public static void main(String[] args) {
-//        int [] weight = new int[]{1,3,4,5};
-//        int [] value = new int[]{1,4,5,7};
-//        int w = 7;
-//
-//        int[][] t = new int[weight.length + 1][w + 1];
-//
-//        minSet(t, t.length, w + 1, -1);
-//        for (int[] row : t) {
-//            System.out.println(Arrays.toString(row));
-//        }
-//        System.out.println("Max profit: " + knapsack(weight, value, w, weight.length, t));
-//
-//    }
+    public static void main(String[] args) {
+        int [] weight = new int[]{1,3,4,5};
+        int [] value = new int[]{1,4,5,7};
+        int w = 7;
 
-    public static int knapsack(int[] wt, int[] val, int w, int n, int[][] t){
-        if(n==0 || w==0) return  0;
+        int[][] t = new int[weight.length + 1][w + 1];
+
+        minSet(t, t.length, w + 1, -1);
+        for (int[] row : t) {
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("Max profit: " + knapsack(weight, value, w, weight.length, t));
+
+    }
+
+    public static int knapsack(int[] wt, int[] val, int w, int n, int[][] t) {
+        if (n == 0 || w == 0) return 0;
 
         if (t[n][w] != -1) return t[n][w];
 
-        if(wt[n-1]<=w){
+        if (wt[n - 1] <= w) {
             return t[n][w] = Math.max(
-                val[n-1] + knapsack(wt,val,w-wt[n-1],n-1, t),
-                knapsack(wt,val,w,n-1, t)
+                    val[n - 1] + knapsack(wt, val, w - wt[n - 1], n - 1, t),
+                    knapsack(wt, val, w, n - 1, t)
             );
         } else {
-            return t[n][w] = knapsack(wt,val,w,n-1, t);
+            return t[n][w] = knapsack(wt, val, w, n - 1, t);
         }
     }
 
@@ -40,18 +40,5 @@ public class KnapsackMemoization {
                 t[i][j] = fill;
             }
         }
-    }
-
-    public static void main(String[] args) {
-
-//        for (int i=0; i<10; i++){
-//            System.out.println(i+"%4="+i%4);
-//        }
-//        System.out.println((int)Character.MAX_VALUE);
-//        System.out.println((int)Character.MIN_VALUE);
-
-
-        System.out.println(Byte.MIN_VALUE);
-        byte b = 011;
     }
 }
---
+++ src/main/java/testing/Knapsack.java
@@ -0,0 +1,72 @@
+package testing;
+
+import java.util.Arrays;
+
+public class Knapsack {
+    public static void main(String[] args) {
+        Knapsack knapsack = new Knapsack();
+        int [] weights = {1,2,3,4,5};
+        int [] prices = {1,10,4,5,7};
+        int bagMaxWeight = 7;
+        int n = 5;
+
+        int [][] t = new int[n+1][bagMaxWeight+1];
+        knapsack.minset(t,-1,t.length,bagMaxWeight+1);
+
+        int res = knapsack.solve(weights,prices,bagMaxWeight,n,t);
+        System.out.println(res);
+
+        knapsack.dpPrint(t);
+
+    }
+
+    public void dpPrint(int[][] t){
+        System.out.println("\n🧾 DP Table (Rows = items, Columns = bag weight):");
+
+        System.out.print("     ");
+        for (int w = 0; w < t[0].length; w++) {
+            System.out.printf("%4d", w);
+        }
+        System.out.println();
+
+        for (int i = 0; i < t.length; i++) {
+            System.out.printf("i=%d ", i);
+            for (int j = 0; j < t[0].length; j++) {
+                if (t[i][j] == -1)
+                    System.out.printf("%4s", "-");
+                else
+                    System.out.printf("%4d", t[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void minset(int [][] t, int val ,int n, int bag){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<bag; j++){
+                t[i][j]=val;
+            }
+        }
+
+    }
+
+    public int solve(int[] weights, int[] prices, int bagMaxWeight, int n,int [][] t){
+        if(n==0 || bagMaxWeight == 0)return 0;
+
+        if(t[n][bagMaxWeight] != -1){
+            return t[n][bagMaxWeight];
+        }
+
+        if(weights[n-1] <= bagMaxWeight){
+            int include = prices[n-1]+ solve(weights,prices,bagMaxWeight-weights[n-1],n-1,t);
+            int exclude =solve(weights,prices,bagMaxWeight,n-1,t);
+            int res = Math.max(include, exclude);
+            t[n][bagMaxWeight] = res;
+            return res;
+        }else{
+            int res = solve(weights,prices,bagMaxWeight,n-1,t);
+            t[n][bagMaxWeight] = res;
+            return res;
+        }
+    }
+}
